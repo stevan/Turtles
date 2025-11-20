0e2b40a66262961fdbcceec16d394af22a32748d
Stevan Little <stevan.little@gmail.com>
2025-11-20 15:36:28 +0100

    lambda

--- src/Corgi.ts
+++ src/Corgi.ts
@@ -2,8 +2,6 @@
 
 type Ident = string
 
-type Word   = { type : 'WORD', ident : Ident  }
-
 type True   = { type : '*T*' }
 type False  = { type : '*F*' }
 
@@ -13,13 +11,18 @@ type Int    = { type : 'INT', value : number  }
 type Float  = { type : 'FLT', value : number  }
 type String = { type : 'STR', value : string  }
 
-type Literal = Int | Float | String | Bool | Word
+type Literal = Int | Float | String | Bool
 
 type Nil  = { type : 'NIL' }
 type Cons = { type : 'CONS', head : Value, tail : List }
+
 type List = Cons | Nil
 
-type Value = Literal | List
+type Lambda = { type : 'LAMBDA', params : List, body: List }
+type Word   = { type : 'WORD', ident : Ident }
+type Var    = { type : 'VAR',  ident : Ident }
+
+type Value = Literal | List | Lambda | Word | Var
 
 // -----------------------------------------------------------------------------
 
@@ -31,14 +34,17 @@ function isNil    (v : any) : v is Nil    { return v.type == 'NIL'  }
 function isCons   (v : any) : v is Cons   { return v.type == 'CONS' }
 function isTrue   (v : any) : v is True   { return v.type == '*T*'  }
 function isFalse  (v : any) : v is False  { return v.type == '*F*'  }
+function isVar    (v : any) : v is Var    { return v.type == 'VAR'  }
+function isLambda (v : any) : v is Lambda { return v.type == 'LAMBDA' }
 function isList   (v : any) : v is List   { return isCons(v) || isNil(v) }
 function isBool   (v : any) : v is Bool   { return isTrue(v) || isFalse(v) }
 function isLiteral (v : any) : v is Literal {
-    return isInt(v) || isFloat(v) || isString(v) || isBool(v) || isWord(v)
+    return isInt(v) || isFloat(v) || isString(v) || isBool(v)
 }
-function isValue (v : any) : v is Value { return isLiteral(v) || isList(v) }
+function isValue (v : any) : v is Value {
+    return isLiteral(v) || isList(v) || isLambda(v) || isWord(v) || isVar(v)
+}
 
-function assertWord    (v : any) : asserts v is Word    { if (!isWord(v))    throw new Error("Not Word")    }
 function assertInt     (v : any) : asserts v is Int     { if (!isInt(v))     throw new Error("Not Int")     }
 function assertFloat   (v : any) : asserts v is Float   { if (!isFloat(v))   throw new Error("Not Float")   }
 function assertString  (v : any) : asserts v is String  { if (!isString(v))  throw new Error("Not String")  }
@@ -48,12 +54,16 @@ function assertBool    (v : any) : asserts v is Bool    { if (!isBool(v))    thr
 function assertNil     (v : any) : asserts v is Nil     { if (!isNil(v))     throw new Error("Not Nil")     }
 function assertCons    (v : any) : asserts v is Cons    { if (!isCons(v))    throw new Error("Not Cons")    }
 function assertList    (v : any) : asserts v is List    { if (!isList(v))    throw new Error("Not List")    }
+function assertLambda  (v : any) : asserts v is Lambda  { if (!isLambda(v))  throw new Error("Not Lambda")  }
+function assertWord    (v : any) : asserts v is Word    { if (!isWord(v))    throw new Error("Not Word")    }
+function assertVar     (v : any) : asserts v is Var     { if (!isVar(v))     throw new Error("Not Var")     }
 function assertLiteral (v : any) : asserts v is Literal { if (!isLiteral(v)) throw new Error("Not Literal") }
 function assertValue   (v : any) : asserts v is Value   { if (!isValue(v))   throw new Error("Not Value")   }
 
 // -----------------------------------------------------------------------------
 
-function Word (ident : Ident) : Word { return { type : 'WORD', ident  } }
+function Word (ident : Ident) : Word { return { type : 'WORD', ident } }
+function Var  (ident : Ident) : Var  { return { type : 'VAR',  ident } }
 
 function True  () : True  { return { type : '*T*' } }
 function False () : False { return { type : '*F*' } }
@@ -65,6 +75,10 @@ function String (value : string) : String { return { type : 'STR', value } }
 function Nil  () : Nil { return { type : 'NIL'} }
 function Cons (head : Value, tail : List) : List {
     return { type : 'CONS', head, tail }
+}
+
+function Lambda (params : List, body : List) : Lambda {
+    return { type : 'LAMBDA', params, body }
 }
 
 // -----------------------------------------------------------------------------
@@ -91,33 +105,89 @@ function tail (l : List) : List {
 
 // -----------------------------------------------------------------------------
 
-class Environment extends Map<Ident, any> {
+type Native = (args : Value[]) => Value
 
-    lookup (word : Word) : any {
-        let bif = env.get(word.ident);
-        if (bif == undefined) throw new Error(`Unable to find ${word.ident} in E`);
+class Environment {
+    public builtins : Map<Ident, Native> = new Map<Ident, Native>();
+    public locals   : Map<Ident, Value>  = new Map<Ident, Value>();
+
+    addBuiltin (ident : Ident, bif : Native) : void {
+        this.builtins.set(ident, bif);
+    }
+
+    lookupBuiltin (word : Word) : any {
+        let bif = this.builtins.get(word.ident);
+        if (bif == undefined) throw new Error(`Unable to find ${word.ident} in E.builtins`);
         return bif;
+    }
+
+    addVar (v : Var, val : Value) : void {
+        this.locals.set(v.ident, val);
+    }
+
+    lookupVar (v : Var) : Value {
+        let value = this.locals.get(v.ident);
+        if (value == undefined) throw new Error(`Unable to find ${v.ident} in E.locals`);
+        return value;
     }
 
 }
 
 // -----------------------------------------------------------------------------
 
+const DEBUG = true;
+const LOG   = (d : number, msg : string, e : any = undefined) => console.log('  '.repeat(d), msg, e ? `<${e.type}>` : '' );
+
 const deCons = (l : List) : Value[] => isNil(l) ? [] : [ head(l), ...deCons(tail(l)) ]
 
-
-function evaluate (expr : Value, env : Environment) : Value {
+function evaluate (expr : Value, env : Environment, depth : number = 0) : Value {
     switch (true) {
     case isCons(expr):
-        let word = head(expr);
-        if (isWord(word)) {
-            let bif  = env.lookup(word);
-            let args = evaluate(tail(expr), env) as List;
-            return bif( ...deCons(args) );
-        } else {
-            return Cons( evaluate(head(expr), env), evaluate(tail(expr), env) as List );
+        if (DEBUG) LOG(depth, 'Got CONS');
+        let top = evaluate(head(expr), env, depth + 1);
+        if (DEBUG) LOG(depth, 'Got CONS HEAD?', top);
+        switch (true) {
+        case isWord(top):
+            if (DEBUG) LOG(depth, 'Got CONS HEAD? WORD?', top);
+            switch(top.ident) {
+            // special form ...
+            case 'lambda':
+                if (DEBUG) LOG(depth, 'Got CONS HEAD? WORD? `lambda (special form)', top);
+                let rest   = tail(expr);
+                let params = head(rest);
+                assertList(params);
+                let body   = head(tail(rest));
+                assertList(body);
+                return Lambda( params, body );
+            default:
+                if (DEBUG) LOG(depth, 'Got CONS HEAD? WORD? *BIF*', top);
+                let bif = env.lookupBuiltin(top);
+                return bif( deCons( evaluate(tail(expr), env, depth + 1) as List ) );
+            }
+        case isLambda(top):
+            if (DEBUG) LOG(depth, 'Got CONS HEAD? LAMBDA', top);
+
+            let params = deCons(top.params);
+            let args   = deCons(evaluate(tail(expr), env, depth + 1) as List);
+
+            for (let i = 0; i < params.length; i++) {
+                let param = params[i];
+                let arg   = args[i];
+                assertVar(param);
+                assertValue(arg);
+                env.addVar(param, arg);
+            }
+
+            return evaluate( top.body, env );
+        default:
+            if (DEBUG) LOG(depth, 'Got CONS *LIST*');
+            return Cons( top, evaluate(tail(expr), env, depth + 1) as List );
         }
+    case isVar(expr):
+        if (DEBUG) LOG(depth, 'Got VAR', expr);
+        return env.lookupVar(expr);
     case isValue(expr):
+        if (DEBUG) LOG(depth, 'Got VAL', expr);
         return expr;
     default:
         throw new Error('WTF!');
@@ -128,14 +198,26 @@ function evaluate (expr : Value, env : Environment) : Value {
 
 let env = new Environment();
 
-env.set('+', (lhs : Value, rhs : Value) : Value => {
+env.addBuiltin('+', (args : Value[]) : Value => {
+    let [ lhs, rhs ] = args;
     assertInt(lhs);
     assertInt(rhs);
     return Int(lhs.value + rhs.value);
 });
 
-let expr = list( Word('+'), Int(20), list( Word('+'), Int(2), list( Word('+'), Int(20), list( Word('+'), Int(2), Int(5) ) ) ) );
+//let expr = list( Word('+'), Int(20), list( Word('+'), Int(2), list( Word('+'), Int(20), list( Word('+'), Int(2), Int(5) ) ) ) );
 
+let expr = list(
+    list(
+        Word('lambda'),
+        list( Var('x'), Var('y') ),
+        list( Word('+'), Var('x'), Var('y') ),
+    ),
+    Int(10),
+    Int(20)
+);
+
+//console.log(JSON.stringify(expr, null, 4));
 console.log(JSON.stringify(evaluate(expr, env), null, 4));
 
 // -----------------------------------------------------------------------------
