614156899f72526a0780d2738aa61ca478f07790
Stevan Little <stevan.little@gmail.com>
2025-11-26 21:32:54 +0100

    foo

--- tests/999-idea.test.ts
+++ tests/999-idea.test.ts
@@ -25,48 +25,54 @@ type Value = Atom | Pair | List
 
 type ParseExpr = Value | ParseExpr[];
 
-const parse = (source : string) : Value => {
+const SPLITTER = /'(?:[^'\\]|\\.)*'|[()]|[^\s()']+/g
 
-    const tokenize = (src : string) : string[] => src.match(/'(?:[^'\\]|\\.)*'|[()]|[^\s()']+/g) ?? []
+const $Parser = {
 
-    const parseTokens = (tokens : string[]) : [ ParseExpr, string[] ] => {
-        let token = tokens[0];
-        if (token == undefined) throw new Error('Undefined Token');
-        let rest = tokens.slice(1);
-        if (token == '(') return parseList( rest, [] );
-        switch (true) {
-        case token == 'true'       : return [ $.bool(true),         rest ];
-        case token == 'false'      : return [ $.bool(false),        rest ];
-        case !isNaN(Number(token)) : return [ $.num(Number(token)), rest ];
-        case token.startsWith('"') : return [ $.str(token),         rest ];
-        default                    : return [ $.sym(token),         rest ];
+    tokenize : (source : string) : List =>
+        $.List.create(...((source.match(SPLITTER) ?? []).map((t) => $.str(t)))),
+
+    parse : (source : string) : Value => {
+        const fastTokenize = (src : string) : string[] => src.match(SPLITTER) ?? []
+
+        const parseTokens = (tokens : string[]) : [ ParseExpr, string[] ] => {
+            let token = tokens[0];
+            if (token == undefined) throw new Error('Undefined Token');
+            let rest = tokens.slice(1);
+            if (token == '(') return parseList( rest, [] );
+            switch (true) {
+            case token == 'true'       : return [ $.bool(true),         rest ];
+            case token == 'false'      : return [ $.bool(false),        rest ];
+            case !isNaN(Number(token)) : return [ $.num(Number(token)), rest ];
+            case token.startsWith('"') : return [ $.str(token),         rest ];
+            default                    : return [ $.sym(token),         rest ];
+            }
         }
+
+        const parseList = (tokens : string[], acc : ParseExpr[]) : [ ParseExpr[], string[] ] => {
+            if (tokens[0] === ')') return [ acc, tokens.slice(1) ];
+            let [ expr, remaining ] = parseTokens( tokens );
+            return parseList( remaining, [ ...acc, expr ] );
+        }
+
+        const buildTree = (expr : ParseExpr) : Value  =>
+            (Array.isArray(expr)) ? $.List.create( ...expr.map(buildTree) ) : expr;
+
+        let [ expr, rest ] = parseTokens( fastTokenize( source ) );
+        return buildTree( expr );
     }
-
-    const parseList = (tokens : string[], acc : ParseExpr[]) : [ ParseExpr[], string[] ] => {
-        if (tokens[0] === ')') return [ acc, tokens.slice(1) ];
-        let [ expr, remaining ] = parseTokens( tokens );
-        return parseList( remaining, [ ...acc, expr ] );
-    }
-
-    const buildTree = (expr : ParseExpr) : Value  =>
-        (Array.isArray(expr)) ? $.list( ...expr.map(buildTree) ) : expr;
-
-    let [ expr, rest ] = parseTokens( tokenize( source ) );
-    return buildTree( expr );
 }
 
 const $ = {
-
     typeOf : (v : Value) : string => v.type,
 
-    isTrue  : (v : Value) : v is Bool => $.isBool(v) && v.value == true,
-    isFalse : (v : Value) : v is Bool => $.isBool(v) && v.value == false,
-    isBool  : (v : Value) : v is Bool => $.typeOf(v) == 'BOOL',
-    isNum   : (v : Value) : v is Num  => $.typeOf(v) == 'NUM',
-    isStr   : (v : Value) : v is Str  => $.typeOf(v) == 'STR',
-    isSym   : (v : Value) : v is Sym  => $.typeOf(v) == 'SYM',
-    isAtom  : (v : Value) : v is Atom => $.isBool(v) || $.isNum(v) || $.isStr(v) || $.isSym(v),
+    isTrue  : (v : Value) : v is Bool    => $.isBool(v) && v.value == true,
+    isFalse : (v : Value) : v is Bool    => $.isBool(v) && v.value == false,
+    isBool  : (v : Value) : v is Bool    => $.typeOf(v) == 'BOOL',
+    isNum   : (v : Value) : v is Num     => $.typeOf(v) == 'NUM',
+    isStr   : (v : Value) : v is Str     => $.typeOf(v) == 'STR',
+    isSym   : (v : Value) : v is Sym     => $.typeOf(v) == 'SYM',
+    isAtom  : (v : Value) : v is Atom    => $.isBool(v) || $.isNum(v) || $.isStr(v) || $.isSym(v),
 
     isPair  : (v : Value) : v is Pair => $.typeOf(v) == 'PAIR',
     isNil   : (v : Value) : v is Nil  => $.typeOf(v) == 'NIL',
@@ -78,37 +84,47 @@ const $ = {
     str  : (value : string)  : Str  => { return { type : 'STR',  value } },
     sym  : (ident : string)  : Sym  => { return { type : 'SYM',  ident } },
 
+    pair : (first : Value, second : Value) : Pair => {
+        return { type : 'PAIR', first, second }
+    },
+
     nil    : () : Nil => { return { type : 'NIL'} },
     cons   : (head : Value, tail : List = $.nil()) : Cons => {
         return { type : 'CONS', head, tail }
     },
 
-    list : (...args : Value[]) : List => {
-        let list : List = $.nil();
-        while (args.length > 0) {
-            list = $.cons( args.pop() as Value, list );
-        }
-        return list;
+    // Lists
+    List : {
+        create : (...args : Value[]) : List => {
+            let list : List = $.nil();
+            while (args.length > 0) {
+                list = $.cons( args.pop() as Value, list );
+            }
+            return list;
+        },
+
+        head : (l : Cons) : Value => l.head,
+        tail : (l : Cons) : List  => l.tail,
+
+        flatten : (l : List) : Value[] => $.isNil(l) ? [] : [ l.head, ...$.List.flatten(l.tail) ],
+        length  : (l : List) : number  => $.isNil(l) ? 0  : 1 + $.List.length(l.tail),
+
+        reduce : (l : List, f : (i : Value, acc : List) => List, acc : List) : List =>
+            $.isNil(l)
+                ? acc
+                : $.List.reduce( l.tail, f, f( l.head, acc ) ),
+
+        map : (l : List, f : (i : Value) => Value) : List =>
+            $.List.reduce(l, (i, acc) => $.cons(f(i), acc), $.nil()),
+
+        grep : (l : List, f : (i : Value) => boolean) : List =>
+            $.List.reduce(l, (i, acc) => f(i) ? acc : $.cons(i, acc), $.nil()),
+
+        find : (l : List, f : (i : Value) => boolean) : Value =>
+            $.isNil(l)
+                ? $.nil()
+                : f(l.head) ? l.head : $.List.find( l.tail, f ),
     },
-
-    head : (l : Cons) : Value => l.head,
-    tail : (l : Cons) : List  => l.tail,
-
-    flatten : (l : List) : Value[] => $.isNil(l) ? [] : [ l.head, ...$.flatten(l.tail) ],
-    length  : (l : List) : number  => $.isNil(l) ? 0  : 1 + $.length(l.tail),
-
-    reduce : (l : List, f : (i : Value, acc : List) => List, acc : List) : List =>
-        $.isNil(l)
-            ? acc
-            : $.reduce( l.tail, f, f( l.head, acc ) ),
-
-    map : (l : List, f : (i : Value) => Value) : List =>
-        $.reduce(l, (i, acc) => $.cons(f(i), acc), $.nil()),
-
-
-    grep : (l : List, f : (i : Value) => boolean) : List =>
-        $.reduce(l, (i, acc) => f(i) ? acc : $.cons(i, acc), $.nil()),
-
 
     pprint : (v : Value) : string => {
         switch (true) {
@@ -119,12 +135,13 @@ const $ = {
         case $.isStr(v)   : return `"${v.value}"`;
         case $.isSym(v)   : return v.ident;
         case $.isPair(v)  : return `(${$.pprint(v.first)} : ${$.pprint(v.second)})`;
-        case $.isCons(v)  : return `(${$.flatten(v).map((i) => $.pprint(i)).join(' ')})`;
+        case $.isCons(v)  : return $.isNil(v.tail)
+            ? `(${$.pprint(v.head)})`
+            : `(${$.pprint(v.head)} ${$.List.flatten(v.tail).map((e) => $.pprint(e)).join(' ')})`;
         default: throw new Error(`Unknown value type (${JSON.stringify(v)})`);
         }
     }
 }
-
 
 // -----------------------------------------------------------------------------
 // Interpeter AST
@@ -133,11 +150,20 @@ const $ = {
 type Tag    = Sym
 type Tagged = Cons
 
-const isTag = (t : Value) : t is Tag => $.isSym(t) && (t.ident == 'Var'
-                                                   ||  t.ident == 'Apply'
-                                                   ||  t.ident == 'Lambda'
-                                                   ||  t.ident == 'Closure');
+const Tags = {
+    Env     : $.sym('Env'),
+    Val     : $.sym('Val'),
+    Var     : $.sym('Var'),
+    Apply   : $.sym('Apply'),
+    Lambda  : $.sym('Lambda'),
+    Closure : $.sym('Closure'),
+}
 
+const isTag = (t : Value) : t is Tag => $.isSym(t) && (t.ident == 'Var'
+                                                    || t.ident == 'Val'
+                                                    || t.ident == 'Apply'
+                                                    || t.ident == 'Lambda'
+                                                    || t.ident == 'Closure');
 
 
 const isTagged    = (v : Value)            : v is Tagged => $.isCons(v) && isTag(v.head);
@@ -146,49 +172,84 @@ const hasTag      = (v : Value, tag : Tag) : v is Tagged => isTagged(v) && $.isS
 
 const tagged = (tag : Tag, tail : List = $.nil()) : Cons => $.cons(tag, tail);
 
-const Tags = {
-    Env     : $.sym('Env'),
-    Var     : $.sym('Var'),
-    Apply   : $.sym('Apply'),
-    Lambda  : $.sym('Lambda'),
-    Closure : $.sym('Closure'),
-}
-
 // ...
-
-const isEnv     = (v : Value) : v is Cons => hasTag(v, Tags.Env);
-
-const isVar     = (v : Value) : v is Cons => hasTag(v, Tags.Var);
-const isApply   = (v : Value) : v is Cons => hasTag(v, Tags.Apply);
-
-const isLambda  = (v : Value) : v is Cons => hasTag(v, Tags.Lambda);
-const isClosure = (v : Value) : v is Cons => hasTag(v, Tags.Closure);
-
-const isTerm  = (v : Value) : v is Cons  => isVar(v) || isApply(v);
-const isValue = (v : Value) : v is Value => isLambda(v) || isClosure(v) || isNotTagged(v);
 
 // -----------------------------------------------------------------------------
 
-const Env     = () : Cons => tagged(Tags.Env);
-const Var     = (symbol : Sym)                : Cons => tagged(Tags.Var,     $.cons(symbol));
-const Apply   = (func   : Value, args : List) : Cons => tagged(Tags.Apply,   $.cons(func,   args));
-const Lambda  = (params : List,  body : Cons) : Cons => tagged(Tags.Lambda,  $.cons(params, body));
-const Closure = (lambda : Cons,  env  : List) : Cons => tagged(Tags.Closure, $.cons(lambda,  env));
+type Env     = Tagged
+type Val     = Tagged
+type Var     = Tagged
+type Apply   = Tagged
+type Lambda  = Tagged
+type Closure = Tagged
+
+type Abs = Var | Lambda
+
+const $Env = {
+    create : (bindings : List = $.nil()) : Env => tagged(Tags.Env, bindings),
+
+    set : (env : Env, symbol : Sym, value : Value) : Env => {
+        return $Env.create( $.cons( $.pair( symbol, value ), env.tail ) )
+    },
+
+    get : (env : Env, symbol : Sym) : Value => {
+        return $.List.find( env.tail, (b) => {
+            if (!($.isPair(b) && $.isSym(b.first))) throw new Error('Expected pair!');
+            return b.first.ident == symbol.ident;
+        })
+    },
+}
+
+// -----------------------------------------------------------------------------
+
+const isEnv     = (v : Value) : v is Env     => hasTag(v, Tags.Env);
+const isVal     = (v : Value) : v is Val     => hasTag(v, Tags.Val);
+const isVar     = (v : Value) : v is Var     => hasTag(v, Tags.Var);
+const isApply   = (v : Value) : v is Apply   => hasTag(v, Tags.Apply);
+const isLambda  = (v : Value) : v is Lambda  => hasTag(v, Tags.Lambda);
+const isClosure = (v : Value) : v is Closure => hasTag(v, Tags.Closure);
+
+const Val     = (value    : Value)               : Val     => tagged(Tags.Val,     $.cons(value));
+const Var     = (symbol   : Sym)                 : Var     => tagged(Tags.Var,     $.cons(symbol));
+const Lambda  = (params   : List,   body : List) : Lambda  => tagged(Tags.Lambda,  $.cons(params, body));
+const Apply   = (func     : Abs,    args : List) : Apply   => tagged(Tags.Apply,   $.cons(func,   args));
+const Closure = (lambda   : Tagged, env  : Env)  : Closure => tagged(Tags.Closure, $.cons(lambda,  env));
+
+const $Compiler = {
+
+    compile : (e : Value) : List => {
+        switch (true) {
+        case $.isSym(e)  : return Var(e);
+        case $.isAtom(e) : return Val(e);
+        case $.isPair(e) : return Val(e);
+        case $.isCons(e) :
+            switch (true) {
+            case $.isSym(e.head): return Apply( Var( e.head ), $Compiler.compile( e.tail ) );
+            case $.isNil(e.tail): return $.cons( $Compiler.compile( e.head ) );
+            default:
+                return $.cons(
+                    $Compiler.compile( e.head ),
+                    $Compiler.compile( e.tail )
+                )
+            }
+        default :
+            throw new Error(`HMMM ${JSON.stringify(e)}`)
+        }
+    },
+
+}
 
 
-let tree = parse('(lambda (x y) (+ x y))');
+let source = `
+    (+ 10 20)
+`;
 
+let parsed = $Parser.parse(source);
+console.log('parsed :', $.pprint(parsed));
 
-console.log('DEPARSE', $.pprint(tree));
-
-
-
-
-
-
-
-
-
+let compiled = $Compiler.compile(parsed);
+console.log('compiled :', $.pprint(compiled));
+//console.log('compiled :', JSON.stringify(compiled, null, 4));
 
 
 
@@ -197,6 +258,49 @@ console.log('DEPARSE', $.pprint(tree));
 
 
 
+// let tree = parse('(lambda (x y) (+ x y))');
+// console.log('DEPARSE', $.pprint(tree));
+// console.log('PARSED', JSON.stringify(tree, null, 4));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+let env = $Env.create();
+console.log($.pprint(env));
+
+env = $Env.set( env, $.sym('x'), $.num(10) );
+console.log($.pprint(env));
+console.log('x = ', $.pprint($Env.get(env, $.sym('x'))));
+console.log('y = ', $.pprint($Env.get(env, $.sym('y'))));
+console.log('z = ', $.pprint($Env.get(env, $.sym('z'))));
+
+env = $Env.set( env, $.sym('y'), $.num(20) );
+console.log($.pprint(env));
+console.log('x = ', $.pprint($Env.get(env, $.sym('x'))));
+console.log('y = ', $.pprint($Env.get(env, $.sym('y'))));
+console.log('z = ', $.pprint($Env.get(env, $.sym('z'))));
+
+env = $Env.set( env, $.sym('z'), $.num(30) );
+console.log($.pprint(env));
+console.log('x = ', $.pprint($Env.get(env, $.sym('x'))));
+console.log('y = ', $.pprint($Env.get(env, $.sym('y'))));
+console.log('z = ', $.pprint($Env.get(env, $.sym('z'))));
+
+*/
 
 
 
