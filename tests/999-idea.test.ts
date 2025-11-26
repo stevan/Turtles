5d5b90d98631e7d89e65e0e0e2fe54bdd2aacead
Stevan Little <stevan.little@gmail.com>
2025-11-26 16:21:32 +0100

    foo

--- tests/999-idea.test.ts
+++ tests/999-idea.test.ts
@@ -20,31 +20,25 @@ type List = Cons | Nil
 type Value = Atom | Pair | List
 
 // -----------------------------------------------------------------------------
-// Type Chekers
-// -----------------------------------------------------------------------------
-
-const isNil   = (v : Value) : v is Nil  => v.type == 'NIL';
-const isBool  = (v : Value) : v is Bool => v.type == 'BOOL';
-const isTrue  = (v : Value) : v is Bool => isBool(v) && v.value == true;
-const isFalse = (v : Value) : v is Bool => isBool(v) && v.value == false;
-const isNum   = (v : Value) : v is Num  => v.type == 'NUM';
-const isStr   = (v : Value) : v is Str  => v.type == 'STR';
-const isSym   = (v : Value) : v is Sym  => v.type == 'SYM';
-const isAtom  = (v : Value) : v is Atom => v.type == 'NIL'
-                                        || v.type == 'BOOL'
-                                        || v.type == 'NUM'
-                                        || v.type == 'STR'
-                                        || v.type == 'SYM';
-
-const isPair = (v : Value) : v is Pair => v.type == 'PAIR';
-const isCons = (v : Value) : v is Cons => v.type == 'CONS';
-const isList = (v : Value) : v is List => v.type == 'NIL' || v.type == 'CONS';
-
-// -----------------------------------------------------------------------------
-// Core AST
+// Core Runtime
 // -----------------------------------------------------------------------------
 
 const $ = {
+
+    typeOf : (v : Value) : string => v.type,
+
+    isTrue  : (v : Value) : v is Bool => $.isBool(v) && v.value == true,
+    isFalse : (v : Value) : v is Bool => $.isBool(v) && v.value == false,
+    isBool  : (v : Value) : v is Bool => $.typeOf(v) == 'BOOL',
+    isNum   : (v : Value) : v is Num  => $.typeOf(v) == 'NUM',
+    isStr   : (v : Value) : v is Str  => $.typeOf(v) == 'STR',
+    isSym   : (v : Value) : v is Sym  => $.typeOf(v) == 'SYM',
+    isAtom  : (v : Value) : v is Atom => $.isBool(v) || $.isNum(v) || $.isStr(v) || $.isSym(v),
+
+    isPair  : (v : Value) : v is Pair => $.typeOf(v) == 'PAIR',
+    isNil   : (v : Value) : v is Nil  => $.typeOf(v) == 'NIL',
+    isCons  : (v : Value) : v is Cons => $.typeOf(v) == 'CONS',
+    isList  : (v : Value) : v is List => $.isNil(v) || $.isCons(v),
 
     bool : (value : boolean) : Bool => { return { type : 'BOOL', value } },
     num  : (value : number)  : Num  => { return { type : 'NUM',  value } },
@@ -64,11 +58,14 @@ const $ = {
         return list;
     },
 
-    flatten : (l : List) : Value[] => isNil(l) ? [] : [ l.head, ...$.flatten(l.tail) ],
-    length  : (l : List) : number  => isNil(l) ? 0  : 1 + $.length( l.tail ),
+    head : (l : Cons) : Value => l.head,
+    tail : (l : Cons) : List  => l.tail,
+
+    flatten : (l : List) : Value[] => $.isNil(l) ? [] : [ l.head, ...$.flatten(l.tail) ],
+    length  : (l : List) : number  => $.isNil(l) ? 0  : 1 + $.length(l.tail),
 
     reduce : (l : List, f : (i : Value, acc : List) => List, acc : List) : List =>
-        isNil(l)
+        $.isNil(l)
             ? acc
             : $.reduce( l.tail, f, f( l.head, acc ) ),
 
@@ -78,25 +75,23 @@ const $ = {
 
     grep : (l : List, f : (i : Value) => boolean) : List =>
         $.reduce(l, (i, acc) => f(i) ? acc : $.cons(i, acc), $.nil()),
-}
 
-// -----------------------------------------------------------------------------
-// AST tools
-// -----------------------------------------------------------------------------
 
-function pprint (v : Value) : string {
-    switch (true) {
-    case isNil(v)   : return '()';
-    case isTrue(v)  : return '#t';
-    case isFalse(v) : return '#f';
-    case isNum(v)   : return v.value.toString();
-    case isStr(v)   : return `"${v.value}"`;
-    case isSym(v)   : return v.ident;
-    case isPair(v)  : return `(${pprint(v.first)} : ${pprint(v.second)})`;
-    case isCons(v)  : return `(${$.flatten(v).map((i) => pprint(i)).join(' ')})`;
-    default: throw new Error(`Unknown value type (${JSON.stringify(v)})`);
+    pprint : (v : Value) : string => {
+        switch (true) {
+        case $.isNil(v)   : return '()';
+        case $.isTrue(v)  : return '#t';
+        case $.isFalse(v) : return '#f';
+        case $.isNum(v)   : return v.value.toString();
+        case $.isStr(v)   : return `"${v.value}"`;
+        case $.isSym(v)   : return v.ident;
+        case $.isPair(v)  : return `(${$.pprint(v.first)} : ${$.pprint(v.second)})`;
+        case $.isCons(v)  : return `(${$.flatten(v).map((i) => $.pprint(i)).join(' ')})`;
+        default: throw new Error(`Unknown value type (${JSON.stringify(v)})`);
+        }
     }
 }
+
 
 // -----------------------------------------------------------------------------
 // Interpeter AST
@@ -105,16 +100,16 @@ function pprint (v : Value) : string {
 type Tag    = Sym
 type Tagged = Cons
 
-const isTag = (t : Value) : t is Tag => isSym(t) && (t.ident == 'Var'
-                                                 ||  t.ident == 'Apply'
-                                                 ||  t.ident == 'Lambda'
-                                                 ||  t.ident == 'Closure');
+const isTag = (t : Value) : t is Tag => $.isSym(t) && (t.ident == 'Var'
+                                                   ||  t.ident == 'Apply'
+                                                   ||  t.ident == 'Lambda'
+                                                   ||  t.ident == 'Closure');
 
 
 
-const isTagged    = (v : Value)            : v is Tagged => isCons(v) && isTag(v.head);
+const isTagged    = (v : Value)            : v is Tagged => $.isCons(v) && isTag(v.head);
 const isNotTagged = (v : Value)            : v is Value  => !isTagged(v);
-const hasTag      = (v : Value, tag : Tag) : v is Tagged => isTagged(v) && isSym(v.head) && v.head.ident == tag.ident;
+const hasTag      = (v : Value, tag : Tag) : v is Tagged => isTagged(v) && $.isSym(v.head) && v.head.ident == tag.ident;
 
 const tagged = (tag : Tag, tail : List = $.nil()) : Cons => $.cons(tag, tail);
 
@@ -148,11 +143,60 @@ const Lambda  = (params : List,  body : Cons) : Cons => tagged(Tags.Lambda,  $.c
 const Closure = (lambda : Cons,  env  : List) : Cons => tagged(Tags.Closure, $.cons(lambda,  env));
 
 
+const tokenize = (src : string) : string[] => src.match(/'(?:[^'\\]|\\.)*'|[()]|[^\s()']+/g) ?? []
+
+const lexer = (tokens : string[]) : any[][] => {
+    let stack : any[][] = [];
+    let expr  : any[] = [];
+    stack.push(expr);
+    while (tokens.length > 0) {
+        let token = tokens.shift();
+        if (token == undefined) throw new Error('WTF! undefined token')
+        //console.log('-'.repeat(80));
+        //console.log('token :', token);
+        //console.log('stack :', stack);
+        //console.log('expr  :', expr);
+
+        switch (true) {
+        case token == '(' :
+            expr = [];
+            stack.push(expr);
+            break;
+        case token == ')' :
+            let next = stack.pop();
+            expr = stack.at(-1) as any[];
+            expr.push(next);
+            break;
+        case token == 'true' :
+            expr.push($.bool(true));
+            break;
+        case token == 'false' :
+            expr.push($.bool(false));
+            break;
+        case !isNaN(Number(token)):
+            expr.push($.num(Number(token)));
+            break;
+        default:
+            if (token.startsWith('"')) {
+                expr.push($.str(token))
+            } else {
+                expr.push($.sym(token));
+            }
+        }
+    }
+
+    return stack;
+}
+
+
+let tokens = tokenize('(10 true ("Hey!" foo))');
+
+console.log('TOKENS', tokens);
+console.log('PARSER', JSON.stringify(lexer(tokens), null, 4));
 
 let term = $.list( $.num(10), $.bool(true), $.str("HEY!"), $.sym("foo") );
 
-
-console.log(pprint(term));
+console.log('AST', $.pprint(term));
 
 
 
